6d960a31a8bced496cd4450fcbfa5aa6f1ad7ae8
Manuel <manuel.barkey@web.de>
2020-01-31 22:52:58 +0100

    rpicam script fixed

--- Application_Raspberry/rpi_cam.py
+++ Application_Raspberry/rpi_cam.py
@@ -9,7 +9,7 @@ from datetime import datetime
 res = (640, 480)
 
 output_dir = os.path.join(
-    os.environ['HOME'], 'object_detection_ncs2', 'output')
+    os.environ['HOME'], 'Bachelor_Arbeit/Application_Raspberry/captures')
 
 
 camera = picamera.PiCamera()
@@ -24,18 +24,12 @@ image = np.empty((res[1], res[0], 3), dtype=np.uint8)
 while True:
     camera.capture(image, 'bgr', use_video_port=True)
 
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('capture', image)
-    cv2.imshow('capture_gray', image_gray)
-
     key = cv2.waitKey(1)
     if key == 113:
         break
     if key == 32:
         iamge_name = 'image_' + datetime.now().strftime("%d-%b-%Y_%H-%M-%S")
         cv2.imwrite(os.path.join(output_dir, iamge_name + '.png'), image)
-        cv2.imwrite(os.path.join(
-            output_dir, iamge_name + '_gray.png'), image_gray)
 
 
 cv2.destroyAllWindows()

cc1a8aa7bca0eebe6c846db2a4b8a0f4afdf04c7
Manuel <manuel.barkey@web.de>
2020-02-06 23:17:06 +0100

    ...

--- Connection/remote_it/ssh_client.py
+++ Connection/remote_it/ssh_client.py
@@ -3,6 +3,8 @@ from scp import SCPClient
 import cv2
 from io import BytesIO
 import os
+import subprocess
+
 
 img = 'test.jpg'
 user_src = 'manuel'
@@ -21,11 +23,14 @@ dest_path = os.path.join(
     '/home', user_dest, 'Bachelor_Arbeit/Connection/remote_it/received', img)
 
 
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(server, port, user_dest, password)
-
-
-with SCPClient(client.get_transport()) as scp:
-    scp.put(src_path, dest_path)
+try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user_dest, password)
+    with SCPClient(client.get_transport()) as scp:
+        scp.put(src_path, dest_path)
+    print('failed with python... trying with bash')
+except:
+    #subprocess.Popen(['scp', ])
+    os.system('scp -P 38564 test.jpg manuel@proxy55.rt3.io:/home/manuel/Bachelor_Arbeit/Connection/remote_it/received')

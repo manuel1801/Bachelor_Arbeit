6091194b85053f0d296dc13c9ca67d1a82e1c098
Manuel <manuel.barkey@web.de>
2020-02-06 22:39:32 +0100

    remote it update

--- Connection/remote_it/ssd_client.py
+++ Connection/remote_it/ssd_client.py
@@ -1,18 +1,31 @@
 import paramiko
 from scp import SCPClient
+import cv2
+from io import BytesIO
+import os
 
-file = open('test.jpg', 'rb').read()
+img = 'test.jpg'
+user_src = 'manuel'
+user_dest = 'pi'
+
+password = 'hiworld'
+
+server = 'proxy50.rt3.io'
+port = '36270'
 
 
-def createSSHClient(server, port, user, password):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, port, user, password)
-    return client
+src_path = os.path.join(
+    'home', user_src, 'Bachelor_Arbeit/Connection/remote_it', img)
+
+dest_path = os.path.join(
+    'home', user_dest, 'Bachelor_Arbeit/Connection/remote_it/received', img)
 
 
-ssh = createSSHClient('proxy50.rt3.io', '36270', 'pi', 'hiworld')
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(server, port, user_dest, password)
+
 
 with SCPClient(ssh.get_transport()) as scp:
-    scp.put('test.jpg')
+    scp.put(src_path, dest_path)

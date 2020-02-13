c8e595e4db6021e5f3931c6cc01b101d73626ce2
Manuel <manuel.barkey@web.de>
2020-02-14 00:56:59 +0100

    test

--- test/connection_ssh_test.py
+++ test/connection_ssh_test.py
@@ -31,7 +31,12 @@ print('TEST: logged in')
 addr = conn.get_device_adress(device_name=my_device)
 print('TEST: got address')
 
-conn_ret = conn.connect(device_address=addr)
+conn_ret = None
+try:
+    conn_ret = conn.connect(device_address=addr)
+except:
+    print('exception 1')
+
 if conn_ret:
     print('TEST: connected sucessfully')
     print(conn_ret)
@@ -43,14 +48,20 @@ if conn_ret:
 else:
     print('TEST: connected errot')
 
-conn_ret = conn.connect(device_address=addr)
+
+conn_ret = None
+try:
+    conn_ret = conn.connect(device_address=addr)
+except:
+    print('exception 2')
+
 if conn_ret:
     print('TEST: connected sucessfully')
     print(conn_ret)
     server, port, conn_id = conn_ret
-    print('sendign file 1')
+    print('sendign file 2')
     conn.send(server, port, user, password, file2, path)
-    print('sendign file 1')
+    print('sendign file 3')
     conn.send(server, port, user, password, file3, path)
     conn.disconnect(addr, conn_id)
 

905fa46a2772b43b63e5d8a96723a0c7b05d027f
Manuel <manuel.barkey@web.de>
2020-02-13 19:42:29 +0100

    remote fix

--- Application_Raspberry/launcher.sh
+++ Application_Raspberry/launcher.sh
@@ -2,9 +2,10 @@
 # launcher.sh
 
 cd /
-cd /home/pi/Bachelor_Arbeit/Application_Raspberry
+#cd /home/pi/Bachelor_Arbeit/Application_Raspberry
+cd /home/pi/Bachelor_Arbeit/Connection/remote_it
 source /opt/intel/openvino/bin/setupvars.sh
-python3 main.py
+python3 connection_ssh_test.py
 cd /
 
 

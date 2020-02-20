c3eb4a5670a609ed7a9c4ae49a779238995efef7
Manuel <manuel.barkey@web.de>
2020-02-20 05:22:29 +0100

    für raspy

--- Application_Raspberry/main.py
+++ Application_Raspberry/main.py
@@ -10,18 +10,18 @@ from time import time
 
 
 # Settings:
-# import picamera.array
-# import picamera
+import picamera.array
+import picamera
 
 
-password = 'helloworld'
+password = 'animalsdetection'
 
-raspi = False
+raspi = True
 
 buffer_size = 200    # zum zwischen speichern wenn infer langsamer stream
 threshhold = 0.7     # Für Detections
 num_requests = 3     # anzahl paralleler inferenz requests, recommended:3
-send_results = False  # falls nein wird local gespeichert)
+send_results = True  # falls nein wird local gespeichert)
 send_email = False   #
 send_all_every = 100  # wie oft alle detections senden (in sekunden, 0 für nie)
 

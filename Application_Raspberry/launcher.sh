4ab7bfdd59efa0f853626d499aa44aee2bb6d86f
manuel1801 <manuel.barkey@web.de>
2020-02-20 01:17:33 +0100

    launcher fixed

---
+++ Application_Raspberry/launcher.sh
@@ -0,0 +1,4 @@
+cd /home/pi/Bachelor_Arbeit/Application_Raspberry
+source /opt/intel/openvino/bin/setupvars.sh
+python3 -u main.py
+cd /
--- Application_Raspberry/launsher.sh
+++
@@ -1,4 +0,0 @@
-cd /home/pi/Bachelor_Arbeit/Application_Raspberry
-source /opt/intl/openvino/bin/setupvars.sh
-/usr/bin/python3 -u main.py
-cd /

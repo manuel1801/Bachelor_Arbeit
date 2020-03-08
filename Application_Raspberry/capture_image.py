5f43efafa6f01083a44713982539d9fbb876c9fd
Manuel <manuel.barkey@web.de>
2020-03-08 17:03:16 +0100

    capture image added

--- Application_Raspberry/capture_image.py
+++ Application_Raspberry/capture_image.py
@@ -12,7 +12,7 @@ camera = picamera.PiCamera()
 camera.resolution = res
 
 night = False
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     night = True
     camera.saturation = -75
 
@@ -22,12 +22,22 @@ time.sleep(1)
 
 image = np.empty((res[1], res[0], 3), dtype=np.uint8)
 
-camera.capture(image, 'bgr', use_video_port=True)
 if night:
-    image_name = 'image_night_' + datetime.now().strftime("%d-%b-%Y_%H-%M-%S") + '.jpg'
+    image_name = 'image_night_' + datetime.now().strftime("%d-%b-%Y_%H-%M-%S")
 else:
-    image_name = 'image_' + datetime.now().strftime("%d-%b-%Y_%H-%M-%S") + '.jpg'
+    image_name = 'image_' + datetime.now().strftime("%d-%b-%Y_%H-%M-%S")
 
-cv2.imwrite(os.path.join('/home/pi/Pictures', image_name), image)
+output_dir = '/home/pi/Pictures'
+n = int(sys.argv[1])
+if n > 1:
+    output_dir = os.path.join(output_dir, image_name)
+    os.mkdir(os.path.join(output_dir))
+
+
+for i in range(n):
+    camera.capture(image, 'bgr', use_video_port=True)
+    cv2.imwrite(os.path.join(output_dir,
+                             str(i) + '_' + image_name + '.jpg'), image)
+
 
 cv2.destroyAllWindows()
